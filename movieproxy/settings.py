5047d5a0ed0c509752d56c966de892a6effa5031
Twixes <dev@twixes.com>
2019-07-17 00:34:52 +0200

    Switch to SQLite instead of Postgres

--- movieproxy/settings.py
+++ movieproxy/settings.py
@@ -41,12 +41,8 @@ WSGI_APPLICATION = 'movieproxy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_PASSWORD', 'movieproxy'),
-        'USER': os.getenv('POSTGRES_USER', 'movieproxy'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
-        'PORT': os.getenv('POSTGRES_PORT', '5432'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, '/db.sqlite3')
     }
 }
 
